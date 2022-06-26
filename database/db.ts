e9fc10c982760780c87606281d117034ae246e64
Felipe Merchan <felipe-sc@hotmail.es>
2022-06-25 21:50:47 -0500

    Conectarnos a MongoDB desde el Restful API

--- database/db.ts
+++ database/db.ts
@@ -20,7 +20,7 @@ const mongooConnection = {
 
 export const connect = async () => {
   if (mongooConnection.isConnected) {
-    console.log('Estamos conectados');
+    console.log('Ya estábamos conectados');
     return;
   }
 
@@ -41,16 +41,16 @@ export const connect = async () => {
     await mongoose.disconnect();
   }
 
-  await mongoose.connect('...');
+  await mongoose.connect(process.env.MONGO_URL || '');
   mongooConnection.isConnected = 1;
-  console.log('Conectado a MongoDB: ', '...');
+  console.log('Conectado a MongoDB: ', process.env.MONGO_URL);
 }
 
 export const disconnect = async () => {
   /* Si el estado es 0 quiere decir que ya estamos
   desconectados por lo cual no es necesario realizar el procedimiendo
   await mongoose.disconnect: */
-  if (mongooConnection.isConnected !== 0) return;
+  if (mongooConnection.isConnected === 0) return;
 
   await mongoose.disconnect();
   console.log('Desconectado de MongoDB');
--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,9 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../database";
 
 type Data = {
-  name: string;
+  message: string;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  /* No debemos realizar este procedimiento si estamos en producción
+  porque no debemos purgar la base de datos en producción: */
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(401).json({ message: 'No tiene acceso a este servicio' });
+  }
+
+  /* Conectarse a la base de datos: */
+  await db.connect();
+  /* En este espacio, entre el método connect y disconect, podemos
+  hacer cualquier petición a la base de datos (lecturas, inserciones, etc)*/
+  /* Desconectarse de la DB: */
+  await db.disconnect();
+
+  res.status(200).json({ message: 'Proceso realizado correctamente' });
 }
