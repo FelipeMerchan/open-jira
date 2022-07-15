6eef50386779db60eeb22ce8c067d6950d69e2d5
Felipe Merchan <felipe-sc@hotmail.es>
2022-07-14 21:54:23 -0500

    Obtener entrada por ID

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -18,10 +18,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 	
 	switch (req.method) {
 		case 'PUT':
-				return updateEntry(req, res);
-
+			return updateEntry(req, res);
+		case 'GET':
+			return getEntry(req, res);
 		default:
-				return res.status(400).json({ message: 'Metodo no existe' })
+			return res.status(400).json({ message: 'Metodo no existe' })
 	}
 }
 
@@ -61,4 +62,18 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 		await db.disconnect();
 		res.status(400).json({ message: error.errors.status.message })
 	}
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { id } = req.query;
+
+	await db.connect();
+	const entryInDB = await Entry.findById(id);
+	await db.disconnect();
+
+	if (!entryInDB) {
+		return res.status(400).json({ message: 'No hay una entrada con el ID: ' + id });
+	}
+
+	return res.status(200).json(entryInDB);
 }
